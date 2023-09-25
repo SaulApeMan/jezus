ef3302669c4c00b038f2414c40322b6046d28984
saulapeman <mihaallenko@gmail.com>
2023-09-25 18:48:50 +0200

    Zadanie11.2.5

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 
-// FIX the error and IMPLEMENT the code
+// FIX the errors
 fn main() {
-    let mut v = Vec::from([1, 2, 3]);
-    for i in 0..5 {
-        println!("{:?}",v.get(i))
-    }
+    let mut v = vec![1, 2, 3];
 
-    for i in 0..5 {
-       // IMPLEMENT the code here...
-       match v.get(i){
-        Some(e) => v[i] = e + 1,
-        None => v.push(i+2)
-       }
-    }
+    let slice1 = &v[..];
+    // Out of bounds will cause a panic
+    // You must use `v.len` here
+    let slice2 = &v[0..v.len()];
     
-    assert_eq!(v, vec![2, 3, 4, 5, 6]);
+    assert_eq!(slice1, slice2);
+    
+    // Slices are read only
+    // Note: slice and &Vec are different
+    let vec_ref: &mut Vec<i32> = &mut v;
+    (*vec_ref).push(4);
+    let slice3 = &v[0..v.len()];
+    
+
+    assert_eq!(slice3, &[1, 2, 3, 4]);
 
     println!("Success!");
 }
