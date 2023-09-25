ca846ac81598939c74c74ae3597a9618f93eb4b2
saulapeman <mihaallenko@gmail.com>
2023-09-25 18:19:54 +0200

    zadanie11.2.4

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,20 @@
-// 11.2.3 Wektory Jezus
-// FILL in the blanks
+
+// FIX the error and IMPLEMENT the code
 fn main() {
-    // Array -> Vec
-    // impl From<[T; N]> for Vec
-    let arr = [1, 2, 3];
-    let v1 = Vec::from(arr);
-    let v2: Vec<i32> = arr.into(); // Metoda into();
- 
-    assert_eq!(v1, v2);
- 
+    let mut v = Vec::from([1, 2, 3]);
+    for i in 0..5 {
+        println!("{:?}",v.get(i))
+    }
+
+    for i in 0..5 {
+       // IMPLEMENT the code here...
+       match v.get(i){
+        Some(e) => v[i] = e + 1,
+        None => v.push(i+2)
+       }
+    }
     
-    // String -> Vec
-    // impl From<String> for Vec
-    let s = "hello".to_string();
-    let v1: Vec<u8> = s.into(); // Nie ma kolizji bo string(wektor) ma u8 typ danych
-
-    let s = "hello".to_string();
-    let v2 = s.into_bytes(); // Inna metoda w wychodzi na to samo
-    assert_eq!(v1, v2);
-
-    // impl<'_> From<&'_ str> for Vec
-    let s = "hello";
-    let v3 = Vec::from(s); // Po prostu from z argumentem STRINGIEM
-    assert_eq!(v2, v3);
-
-    // Iterators can be collected into vectors
-    let v4: Vec<i32> = [0; 10].into_iter().collect(); // przez zebranie wskaznikow podobnie do from?
-    assert_eq!(v4, vec![0; 10]);
+    assert_eq!(v, vec![2, 3, 4, 5, 6]);
 
     println!("Success!");
- }
+}
